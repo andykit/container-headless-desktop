51033a049f0080be1a5230ceb1e341def5299e75
andypau <42061515+andykit@users.noreply.github.com>
2021-03-26 16:46:33 +0800

    Update start_display.sh

--- scripts/start_display.sh
+++ scripts/start_display.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-if [[ ! -f ~/display.env ]]
-then
-	display=`ls -1 /var/run/xpra/* 2>/dev/null | sed -E -e '/:/d' -e 's/^.*-([0-9]+)$/\1/' | sort -r | head -n 1`
-	if [[ -z "$display" ]]
-	then
-		display=100
-	else
-		display=$((display+1))
-	fi
-	echo starting display :$display ...
-	xpra start --start-via-proxy=no :$display
-	export DISPLAY=:$display
-	echo "export DISPLAY=:$display" > ~/display.env
-	touch /var/run/xpra/"$CONTAINER_LABEL-$HOSTNAME:$display.desc"
-	while [[ ! -S /var/run/xpra/display-$display ]]
-	do
-		echo "waiting ..."
-		sleep 1
-	done
-else
-	. ~/display.env
-fi
+# if [[ ! -f ~/display.env ]]
+# then
+# 	display=`ls -1 /var/run/xpra/* 2>/dev/null | sed -E -e '/:/d' -e 's/^.*-([0-9]+)$/\1/' | sort -r | head -n 1`
+# 	if [[ -z "$display" ]]
+# 	then
+# 		display=100
+# 	else
+# 		display=$((display+1))
+# 	fi
+# 	echo starting display :$display ...
+# 	xpra start --start-via-proxy=no :$display
+# 	export DISPLAY=:$display
+# 	echo "export DISPLAY=:$display" > ~/display.env
+# 	touch /var/run/xpra/"$CONTAINER_LABEL-$HOSTNAME:$display.desc"
+# 	while [[ ! -S /var/run/xpra/display-$display ]]
+# 	do
+# 		echo "waiting ..."
+# 		sleep 1
+# 	done
+# else
+# 	. ~/display.env
+# fi
+
+xpra start --bind-tcp=0.0.0.0:9876 --html=on --start-child=xterm --exit-with-children=yes -screen 0 1920x1080x24+32
